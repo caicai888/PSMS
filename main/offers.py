29ef38d7c65bcc9862e0d719acf65a5b26ba8b83
liyin <liyin@liyindeMacBook-Pro.local>
2017-01-05 13:48:31 +0800

    delete path9

--- main/offers.py
+++ main/offers.py
@@ -723,9 +723,10 @@ def updateContryTime():
 #
 @offers.route('/<path>')
 def today(path):
-    base_dir = os.path.abspath(__file__)
+    # base_dir = os.path.abspath(__file__)
+    base_dir = os.path.abspath(os.path.join(os.path.dirname('index.html'), os.path.pardir))
     print "+++++"*20
-    print type(base_dir)
+    print base_dir
     print path
     resp = make_response(open(os.path.join(base_dir, path)))
     resp.headers["Content-type"] = "application/json;charset=UTF-8"
