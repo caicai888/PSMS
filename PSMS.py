6e8f970d1f5116c5b8ee130d3c9b110cddd9a645
liyin <liyin@liyindeMacBook-Pro.local>
2017-01-06 10:22:38 +0800

    add thread

--- PSMS.py
+++ PSMS.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # coding=utf-8
 from main import create_app
+# from werkzeug.contrib.fixers import ProxyFix
 
 config = "default"
 app = create_app(config)
+# app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5555)
+    app.run(host="0.0.0.0", port=5555,threaded=True)
 
