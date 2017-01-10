a1fe4b9ba063259fc54dfd2d8ef386babc6e2133
ZivLi <lijiaxing@newborn-town.com>
2017-01-10 14:50:16 +0800

    google

--- main/config.py
+++ main/config.py
@@ -6,6 +6,7 @@ def random_string():
 
 class Config:
     SECRET_KEY = random_string()
+    BASE_API_URL = "http://api.beta.dev.careerdream.org/core/v1/"
 
     @staticmethod
     def init_app(app):
