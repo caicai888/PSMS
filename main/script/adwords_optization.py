0673f8a463452113d2ef428545365e6edf35c936
liyin <liyin@liyindeMacBook-Pro.local>
2017-03-22 15:50:44 +0800

    get adwords optname

--- main/script/adwords_optization.py
+++ main/script/adwords_optization.py
@@ -23,29 +23,29 @@ for i in adwords_results:
     if i[0] not in accountIds:
         accountIds.append(i[0])
 print len(accountIds)
-# client = adwords.AdWordsClient.LoadFromStorage()
-# for j in accountIds:
-#     client.SetClientCustomerId(j)
-#     tempf = tempfile.NamedTemporaryFile(delete=True)
-#     REPORT = 'ACCOUNT_PERFORMANCE_REPORT'
-#     fields = ['AccountDescriptiveName']
-#     column_list = ','.join(fields)
-#     report_downloader = client.GetReportDownloader(version='v201609')
-#     report_query_string = ('SELECT %s FROM %s' % (column_list, REPORT))
-#     report_downloader.DownloadReportWithAwql(report_query_string, 'CSV', tempf, skip_report_header=True, skip_column_header=False,
-#                                              skip_report_summary=False)
-#     tempf.seek(0)
-#     with open(tempf.name, 'r') as csv_file:
-#         reader = csv.DictReader(csv_file)
-#         for read in reader:
-#             if read['Account'] != "Total":
-#                 print read['Account']
-#                 print "+++"*10
-#                 account_name = read['Account'].split('_')
-#                 for n in account_name:
-#                     if "66" in n:
-#                         update_opt = "update adwords set optName='%s' where account_id='%s'" % (n, j)
-#                         cursor.execute(update_opt)
-#                         db.commit()
-#
-# print "ok"
+client = adwords.AdWordsClient.LoadFromStorage()
+for j in accountIds:
+    client.SetClientCustomerId(j)
+    tempf = tempfile.NamedTemporaryFile(delete=True)
+    REPORT = 'ACCOUNT_PERFORMANCE_REPORT'
+    fields = ['AccountDescriptiveName']
+    column_list = ','.join(fields)
+    report_downloader = client.GetReportDownloader(version='v201609')
+    report_query_string = ('SELECT %s FROM %s' % (column_list, REPORT))
+    report_downloader.DownloadReportWithAwql(report_query_string, 'CSV', tempf, skip_report_header=True, skip_column_header=False,
+                                             skip_report_summary=False)
+    tempf.seek(0)
+    with open(tempf.name, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for read in reader:
+            if read['Account'] != "Total":
+                print read['Account']
+                print "+++"*10
+                account_name = read['Account'].split('_')
+                for n in account_name:
+                    if "66" in n:
+                        update_opt = "update adwords set optName='%s' where account_id='%s'" % (n, j)
+                        cursor.execute(update_opt)
+                        db.commit()
+
+print "ok"
