77792e5b4695f7b71689e8c4c311a36be9f2fd61
xynewborntown <wangxuezhi@newborntown.com>
2017-03-29 14:26:17 +0800

    modeify models , accountRebate

--- main/accountRebate.py
+++ main/accountRebate.py
@@ -9,13 +9,23 @@ accountRebate = Blueprint('accountRebate', __name__)
 
 @accountRebate.route('/api/rebate/create', methods=['POST','GET'])
 def create_rebate():
+    print request.method
     if request.method == "POST":
         data = request.get_json(force=True)
-        accountId = data['accountId']
+        print 'Get data from web:',data
+        accountName = data['accountName']
         scale = data['scale']
+        platform = data['platform']
+        companyName = data['companyName']
+        company_address = data['company_address']
+        bank_account = data['bank_account']
+        concordat_code = data['concordat_code']
+        keywords = data['keywords']
+        remark = data['remark']
         rebateId = data['rebateId']
+
         if rebateId == "":
-            rebate = Rebate(accountId,float(scale))
+            rebate = Rebate(accountName, float(scale),keywords,companyName,company_address,bank_account,concordat_code,remark,platform)
             db.session.add(rebate)
             db.session.commit()
             db.create_all()
@@ -34,15 +44,23 @@ def create_rebate():
 def show_rebate():
     rebates = Rebate.query.all()
     rebate_lists = []
+    print '************',rebates
     for i in rebates:
         rebate_lists += [
             {
-                "accountId": i.accountId,
+                "accountName": i.accountName,
                 "scale": str(i.scale),
-                "id": i.id
+                "id": i.id,
+                "keywords": i.keywords,
+                "companyName": i.companyName,
+                "address": i.address,
+                "bank_account": i.bank_account,
+                "concordat_code": i.concordat_code,
+                "remark": i.remark,
+                "platform": i.platform
             }
         ]
-
+    print 'Return to webdatas : ', rebate_lists
     return json.dumps({"code":200,"message":"success","results": rebate_lists})
 @accountRebate.route('/api/rebate/show/<id>', methods=['GET','POST'])
 def show_rebate_detail(id):
