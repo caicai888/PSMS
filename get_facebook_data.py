02088baa8c8aa715c929d046c246d54498fac43c
liyin <liyin@liyindeMacBook-Pro.local>
2017-03-30 22:39:11 +0800

    live chat

--- get_facebook_data.py
+++ get_facebook_data.py
@@ -19,8 +19,8 @@ start_date = datetime.datetime.strftime(start_date, '%Y-%m-%d')
 
 db = MySQLdb.connect("localhost","root","chizicheng521","psms",charset='utf8')
 cursor = db.cursor()
-# sql = "select offer_id,facebook_keywords from advertisers where type='facebook' and offer_id in (select id from offer where status != 'deleted')"
-sql = "select offer_id,facebook_keywords from advertisers where type='facebook' and offer_id=28"
+sql = "select offer_id,facebook_keywords from advertisers where type='facebook' and offer_id in (select id from offer where status != 'deleted')"
+# sql = "select offer_id,facebook_keywords from advertisers where type='facebook' and offer_id=29"
 cursor.execute(sql)
 results = cursor.fetchall()
 
@@ -107,8 +107,6 @@ for i in results:
 
     if time_ranges != []:
         for campaignId in advertise_series:
-            print "+++"*10
-            print campaignId
             url = "https://graph.facebook.com/v2.8/" + str(campaignId) + "/insights"
             params = {
                 "access_token": accessToken,
@@ -254,8 +252,6 @@ for i in results:
                 ele['clicks'] = float(ele['clicks'])
                 tempList.append(key)
                 clicks_list.append(ele)
-        print "%%%"*10
-        print conversions_list
         # conversions_list_unique = []
         # for j in conversions_list:
         #     if j not in conversions_list_unique:
@@ -440,22 +436,22 @@ for i in results:
                 cursor.execute(update_sql)
                 db.commit()
 
-# if (datetime.datetime.now()+datetime.timedelta(hours=8)).strftime('%H:%M') >= "08:00":
-#     mail_body = "facebook data finished"
-#     mail_from = "ads_reporting@newborntown.com"
-#     mail_to = "liyin@newborntown.com"
-#     msg = MIMEMultipart()
-#     body = MIMEText(mail_body)
-#     msg.attach(body)
-#     msg['From'] = mail_from
-#     msg['To'] = mail_to
-#     msg['date'] = time.strftime('%Y-%m-%d')
-#     msg['Subject'] = "get facebook Data finished"
-#     smtp = smtplib.SMTP()
-#     smtp.connect('smtp.exmail.qq.com', 25)
-#     smtp.ehlo()
-#     smtp.starttls()
-#     smtp.ehlo()
-#     smtp.login('ads_reporting@newborntown.com', '5igmKD3F0cLScrS5')
-#     smtp.sendmail(mail_from, mail_to, msg.as_string())
-#     smtp.quit()
+if (datetime.datetime.now()+datetime.timedelta(hours=8)).strftime('%H:%M') >= "08:00":
+    mail_body = "facebook data finished"
+    mail_from = "ads_reporting@newborntown.com"
+    mail_to = "liyin@newborntown.com"
+    msg = MIMEMultipart()
+    body = MIMEText(mail_body)
+    msg.attach(body)
+    msg['From'] = mail_from
+    msg['To'] = mail_to
+    msg['date'] = time.strftime('%Y-%m-%d')
+    msg['Subject'] = "get facebook Data finished"
+    smtp = smtplib.SMTP()
+    smtp.connect('smtp.exmail.qq.com', 25)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login('ads_reporting@newborntown.com', '5igmKD3F0cLScrS5')
+    smtp.sendmail(mail_from, mail_to, msg.as_string())
+    smtp.quit()
