ab77e52a4e78e7faae97cacb82c9ed9d360569b9
xynewborntown <wangxuezhi@newborntown.com>
2017-04-05 19:36:39 +0800

    tijiao

--- main/config.py
+++ main/config.py
@@ -27,7 +27,7 @@ class Config:
 class ProductionConfig(Config):
     DEBUG = True
     MYSQL_USER = "root"
-    MYSQL_PASS = "wangxuezhi"
+    MYSQL_PASS = "chizicheng521"
     MYSQL_HOST = "localhost"
     MYSQL_PORT = "3306"
     MYSQL_DB = "psms"
