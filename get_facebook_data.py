8b1a0013a331c7b69a6ac8c5c29d482081174474
liyin <liyin@liyindeMacBook-Pro.local>
2017-04-05 20:07:54 +0800

    fb

--- get_facebook_data.py
+++ get_facebook_data.py
@@ -19,8 +19,8 @@ start_date = datetime.datetime.strftime(start_date, '%Y-%m-%d')
 
 db = MySQLdb.connect("localhost","root","chizicheng521","psms",charset='utf8')
 cursor = db.cursor()
-sql = "select offer_id,facebook_keywords from advertisers where type='facebook' and offer_id in (select id from offer where status != 'deleted')"
-# sql = "select offer_id,facebook_keywords from advertisers where type='facebook' and offer_id=2"
+# sql = "select offer_id,facebook_keywords from advertisers where type='facebook' and offer_id in (select id from offer where status != 'deleted')"
+sql = "select offer_id,facebook_keywords from advertisers where type='facebook' and offer_id=19"
 cursor.execute(sql)
 results = cursor.fetchall()
 
@@ -359,7 +359,9 @@ for i in results:
                         price = offer_price
                     else:
                         price = history_result[0]
-
+                print price
+                print conversion
+                print "++++"*10
                 revenue_list += [
                     {
                         "country": country,
