f9dcf9a9bce893576878b2349e20e7655b7d0c81
liyin <liyin@liyindeMacBook-Pro.local>
2017-04-05 17:25:03 +0800

    country adwords uac

--- main/script/adwords_uac.py
+++ main/script/adwords_uac.py
@@ -30,8 +30,8 @@ class PSMSOffer(object):
 
     def get_campaigns(self):
         cursor = conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-        # query_string = "select offer_id, adwords_notuac, adwords_uac from advertisers where type = 'adwords' and offer_id in (select id from offer where status != 'deleted') and offer_id != 126"
-        query_string = "select offer_id, adwords_notuac, adwords_uac from advertisers where type = 'adwords' and offer_id=126"
+        query_string = "select offer_id, adwords_notuac, adwords_uac from advertisers where type = 'adwords' and offer_id in (select id from offer where status != 'deleted')"
+        # query_string = "select offer_id, adwords_notuac, adwords_uac from advertisers where type = 'adwords' and offer_id=126"
         try:
             cursor.execute(query_string)
         finally:
@@ -131,10 +131,7 @@ class AdwordsUac(AdwordsSQL):
                 for read in reader:
                     if read['Campaign ID'] != 'Total':
                         if self.is_uac == False:
-                            print read['Campaign ID']
                             countryNumber = read["Country/Territory"]
-                            print countryNumber
-                            print "&&&&&"
                             country_sql = "select countryName from adwordsGeo where countryNumber='%s'"%(countryNumber)
                             cursor.execute(country_sql)
                             country_result = cursor.fetchone()
